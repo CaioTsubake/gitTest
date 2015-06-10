0eda9490d3238ac5f1dd6544668b36c2356ba5c1
Caio Tsubake <Caio Tsubake@CaioTsubake>
2015-06-10 18:29:23 -0300

    Criado validação
    A aplicação já consegue indentificar um usuário salvo no BD pelo
    username e password

--- test_1/app/controllers/User.java
+++ test_1/app/controllers/User.java
@@ -2,8 +2,13 @@ package controllers;
 
 
 import java.util.List;
+
+import com.avaje.ebean.Ebean;
+
 import models.UserModel;
+import play.Logger;
 import play.data.Form;
+import play.data.Form.Field;
 import play.db.ebean.Model;
 import play.mvc.*;
 import views.html.*;
@@ -19,13 +24,29 @@ public class User extends Controller{
 	public static Result reg() {
 		return ok(register.render());
 	}
-
+	
+	public static Result login() {
+		UserModel formUser = Form.form(UserModel.class).bindFromRequest().get();
+		
+		
+		UserModel user = Ebean.find(UserModel.class)
+				.where()
+					.eq("username", formUser.username)
+				.findUnique();
+		
+		
+		if(user != null){
+			if(formUser.password.equals(user.password)){
+				return redirect(routes.Application.index());
+			}
+		}
+		
+		return redirect(routes.User.index());
+	}
+	
 	public static Result addUser() {
 		UserModel user = Form.form(UserModel.class).bindFromRequest().get();
 		user.save();
-		System.out.print("caio");
-		System.out.print(user);
-		//return ok();
 		return redirect(routes.User.reg());
 	}
 	
