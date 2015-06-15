333f088a2cd7f259afa146ecc8be6ec6adc2dfa2
Caio Tsubake <Caio Tsubake@CaioTsubake>
2015-06-15 11:54:52 -0300

    Campo de Comentário criado

--- test_1/conf/evolutions/default/1.sql
+++ test_1/conf/evolutions/default/1.sql
@@ -3,6 +3,13 @@
 
 # --- !Ups
 
+create table comment_model (
+  id                        varchar(255) not null,
+  posted_at                 timestamp,
+  content                   varchar(255),
+  constraint pk_comment_model primary key (id))
+;
+
 create table user_model (
   id                        varchar(255) not null,
   username                  varchar(255),
@@ -10,6 +17,8 @@ create table user_model (
   repeat_password           varchar(255),
   constraint pk_user_model primary key (id))
 ;
+
+create sequence comment_model_seq;
 
 create sequence user_model_seq;
 
@@ -20,9 +29,13 @@ create sequence user_model_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists comment_model;
+
 drop table if exists user_model;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists comment_model_seq;
 
 drop sequence if exists user_model_seq;
 
